3cbd6cf414a837e1e3727e1156d66d95067b1c16
darkestmidnight <elly.richardson18@gmail.com>
2019-09-05 00:00:25 -0500

    Added a cell class for the Schedule View

---
+++ ProjectFND/components/cells/CalendarCell.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarCell.swift
+//  ProjectFND
+//
+//  Created by Elly Richardson on 9/4/19.
+//  Copyright © 2019 EllyRichardson. All rights reserved.
+//
+//  Parts of this code was taken from CalendarControlUsingJTAppleCalenader
+//  project by anoop4real.
+//
+
+import JTAppleCalendar
+
+class CalendarCell: JTAppleCell {
+    
+    // THIS is still not in the repo
+    @IBOutlet weak var selectedView: UIView!
+    @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var dateLabel: UILabel!
+}
