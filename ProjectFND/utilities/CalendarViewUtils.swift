2e57036d9a62a19628d5827bce478cd122d58a5e
darkestmidnight <elly.richardson18@gmail.com>
2020-03-10 17:28:54 -0500

    Some better fixes for the Preview calendar view Selection and alignments

--- ProjectFND/utilities/CalendarViewUtils.swift
+++ ProjectFND/utilities/CalendarViewUtils.swift
@@ -15,40 +15,40 @@ class CalendarViewUtils {
     static func showCellIndicators(cell: CalendarCell, onProgress: Bool, finished: Bool, overdue: Bool) -> CalendarCell {
         // Yellow indicator only
         if onProgress == true && finished == false && overdue == false {
-            cell.bottomIndicator.isHidden = false
+            cell.topIndicator.isHidden = false
             cell.createIndicators(createIndicator: true, indicatorType: 1)
         }
             // Green indicator only
         else if onProgress == false && finished == true && overdue == false {
-            cell.bottomIndicator.isHidden = false
+            cell.topIndicator.isHidden = false
             cell.createIndicators(createIndicator: true, indicatorType: 2)
         }
             // Orange indicator only
         else if onProgress == false && finished == false && overdue == true {
-            cell.bottomIndicator.isHidden = false
+            cell.topIndicator.isHidden = false
             cell.createIndicators(createIndicator: true, indicatorType: 3)
         }
             // Yellow and Green indicator
         else if onProgress == true && finished == true && overdue == false {
-            cell.topIndicator.isHidden = false
-            cell.bottomIndicator.isHidden = false
+            cell.topLeftIndicator.isHidden = false
+            cell.topRightIndicator.isHidden = false
             cell.createIndicators(createIndicator: true, indicatorType: 4)
         }
             // Yellow and Orange indicator
         else if onProgress == true && finished == false && overdue == true {
-            cell.bottomIndicator.isHidden = false
-            cell.topIndicator.isHidden = false
+            cell.topRightIndicator.isHidden = false
+            cell.topLeftIndicator.isHidden = false
             cell.createIndicators(createIndicator: true, indicatorType: 5)
         }
             // Green and Orange indicator
         else if onProgress == false && finished == true && overdue == true {
-            cell.bottomIndicator.isHidden = false
-            cell.topIndicator.isHidden = false
+            cell.topLeftIndicator.isHidden = false
+            cell.topRightIndicator.isHidden = false
             cell.createIndicators(createIndicator: true, indicatorType: 6)
         }
             // Yellow, Green, and Orange indicator
         else if onProgress == true && finished == true && overdue == true {
-            cell.bottomIndicator.isHidden = false
+            cell.topIndicator.isHidden = false
             cell.topRightIndicator.isHidden = false
             cell.topLeftIndicator.isHidden = false
             cell.createIndicators(createIndicator: true, indicatorType: 0)
